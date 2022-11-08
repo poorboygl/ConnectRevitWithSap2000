159245cc796c58f4c465560f45b693f6446a6ba8
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2022-11-08 22:15:53 +0700

    first commit

---
+++ myFirstPlugin/Command.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using System.Windows.Forms;
+using Form = System.Windows.Forms.Form;
+
+
+namespace myFirstPlugin
+{
+    /// <summary>
+    /// Implements revit add-in IExternalCommand interface
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class Command : IExternalCommand
+    {
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIApplication uiApp = commandData.Application;
+            Document doc = uiApp.ActiveUIDocument.Document;
+
+
+            using (Form form = new Form1(doc))
+            {
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    return Result.Succeeded;
+                }
+                else
+                {
+                    return Result.Cancelled;
+                }
+            }
+                       
+        }       
+
+    }
+
+}
---
+++ myFirstPlugin/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ARB=Autodesk.Revit.DB;
+using Form = System.Windows.Forms.Form;
+using Sap2000;
+
+namespace myFirstPlugin
+{
+    public partial class Form1 : Form
+    {
+        ARB.Document _doc;
+  
+      
+
+        public Form1(ARB.Document doc)
+        {
+            InitializeComponent();
+            _doc = doc;
+
+        }
+
+        private void wall_count_Click(object sender, EventArgs e)
+        {
+            Sap2000.SapObject sapObject = new Sap2000.SapObject();
+            sapObject.ApplicationStart(eUnits.kN_mm_C, true);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
